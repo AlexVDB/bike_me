8bc8e3e62c47cffd05d567b5a155d8805ec8388a
Thibault <belair.thibault@gmail.com>
2019-08-20 15:48:19 +0200

    Added associations to all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :owned_bikes, class_name: 'Bike', foreign_key: :owner_id
+  has_many :reservations
+  has_many :bikes, through: :reservations, class_name: 'Bike', foreign_key: :owner_id
 end
