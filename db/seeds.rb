1f0a037ffb76b088599113cafcb553aa5ea46a5a
Alex <alexandra.vandenbilcke@gmail.com>
2019-08-20 20:43:14 +0200

    Added users and bikes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users_attributes = [
+  {
+    name:'Toto',
+    email: 'toto@gmail.com',
+    localisation: 'Lyon'
+  },
+  {
+    name:'Tata',
+    email: 'tata@gmail.com',
+    localisation: 'Paris'
+  },
+  {
+    name:'Titi',
+    email: 'titi@gmail.com',
+    localisation: 'Marseille'
+  }
+]
+
+bikes_attributes = [
+  {
+    title: 'VTT amélioré',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quis inventore molestiae illum, facilis sequi, illo quos aut repellat nostrum ipsam minus. Facere perspiciatis fuga ratione repellendus, voluptatum sit provident.',
+    category: 'VTT',
+    motor: true,
+    price: 100,
+    user_id: 1,
+  },
+  {
+    title: 'Mon premier vélo',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quis inventore molestiae illum, facilis sequi, illo quos aut repellat nostrum ipsam minus. Facere perspiciatis fuga ratione repellendus, voluptatum sit provident.',
+    category: 'Enfant',
+    motor: false,
+    price: 70,
+    user_id: 2,
+  },
+  {
+    title: 'Tandem pour 10 personnes',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quis inventore molestiae illum, facilis sequi, illo quos aut repellat nostrum ipsam minus. Facere perspiciatis fuga ratione repellendus, voluptatum sit provident.',
+    category: 'Tandem',
+    motor: false,
+    price: 150,
+    user_id: 1,
+  },
+  {
+    title: 'Vélo vintage (dans son jus)',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quis inventore molestiae illum, facilis sequi, illo quos aut repellat nostrum ipsam minus. Facere perspiciatis fuga ratione repellendus, voluptatum sit provident.',
+    category: 'Vintage',
+    motor: false,
+    price: 30,
+    user_id: 3,
+  },
+  {
+    title: 'Vélo sans roues',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quis inventore molestiae illum, facilis sequi, illo quos aut repellat nostrum ipsam minus. Facere perspiciatis fuga ratione repellendus, voluptatum sit provident.',
+    category: 'VTT',
+    motor: true,
+    price: 10,
+    user_id: 3,
+  }
+]
