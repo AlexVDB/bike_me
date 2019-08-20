d8890b25d4ac09728f26e203684a9fd1d556d68e
Alex <alexandra.vandenbilcke@gmail.com>
2019-08-20 14:42:56 +0200

    Added Bikes controller & routes without ressource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'bikes/search'
+  get 'bikes/index'
+  get 'bikes/show'
+  get 'bikes/new'
+  get 'bikes/create'
+  get 'bikes/manage'
+  get 'bikes/edit'
+  get 'bikes/update'
+  get 'bikes/destroy'
   get '/profil', to: 'users#profil'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
