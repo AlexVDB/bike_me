0f7a2830c35f341bc243277b044b910ea1cb196d
Thibault <belair.thibault@gmail.com>
2019-08-20 18:42:06 +0200

    add gem device

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,23 +1,9 @@
 class Bike < ApplicationRecord
   belongs_to :user
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-validates :tile, presence: true, allow_blank: false
-validates :description, presence: true, allow_blank: false
-validates :category, presence: true, uniqueness: true, allow_blank: false
-validates :motor, presence: true, uniqueness: true, allow_blank: false
-validates :price, presence: true, allow_blank: false
-
+  validates :tile, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+  validates :category, presence: true, uniqueness: true, allow_blank: false
+  validates :motor, presence: true, uniqueness: true, allow_blank: false
+  validates :price, presence: true, allow_blank: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ApplicationRecord
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :owned_bikes, class_name: 'Bike', foreign_key: :owner_id
   has_many :reservations
   has_many :bikes, through: :reservations, class_name: 'Bike', foreign_key: :owner_id
 
-
-
   validates :name, uniqueness: true, presence: true, allow_blank: false
   validates :email, uniqueness: true, presence: true, allow_blank: false
   validates :localisation, uniqueness: true, presence: true, allow_blank: false
-
-
-
 end
