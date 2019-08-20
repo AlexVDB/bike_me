9804321d6b870087df982d9e958a8f21c7c2f4fe
Thibault <belair.thibault@gmail.com>
2019-08-20 17:26:12 +0200

    methods reservations added and route deleted

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,23 @@
 class ReservationsController < ApplicationController
   def show
-  end
-
-  def new
+    @reservation = Reservation.find(params[:id])
   end
 
   def create
+    @bike = Bike.find(params[:bike_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.bike = @bike
+    @reservation.user = current_user
+    if @reservation.save
+      redirect_to @reservation, notice: 'Reservation was successfully booked!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,23 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :bike
   belongs_to :user
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   validates :date, presence: true
-
   validates_uniqueness_of :user, scope: :bike_id
-
 end
