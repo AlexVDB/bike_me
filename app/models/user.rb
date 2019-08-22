795aaaa33a46519aa598847707ffcb5d9bc497dc
cesar <cesarlewagon@gmail.com>
2019-08-22 14:58:12 +0200

    added geocode in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, presence: true, allow_blank: false
   validates :email, uniqueness: true, presence: true, allow_blank: false
   validates :localisation, uniqueness: true, presence: true, allow_blank: false
+
+  geocoded_by :localisation
+  after_validation :geocode, if: :will_save_change_to_address?
 end
