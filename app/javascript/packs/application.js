9f551f675a7166d99d3604fea718b1f5b4281db4
Thibault <belair.thibault@gmail.com>
2019-08-22 19:24:45 +0200

    flatpicker added to home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
+// CSS
+import 'flatpickr/dist/flatpickr.css';
+
+
 import "bootstrap";
+import flatpickr from "flatpickr";
+
+const flatInputs = document.querySelectorAll('.flatpickr');
+
+flatInputs.forEach((input) => {
+  flatpickr(input, {});
+})
